935ea1dd14fd6f422349357c8e1721c65c23d1ae
Edd-Chav <eduardo_3141240140@utd.edu.mx>
2026-03-04 22:55:43 -0600

    Actualización de los archivos de React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Metodologias from './Metodologias';
 
 // 👇 1. IMPORTAS TU IMAGEN AQUÍ (Pon el nombre real de tu archivo)
-import miFotoLocal from './yo.jpg';
+import miFotoLocal from './yo.jpeg';
 
 function App() {
+  const [showMetodologias, setShowMetodologias] = useState(false);
   return (
     <div className="main-container">
       <div className="content-box">
-        
-        {/* 👇 2. USAS LA VARIABLE IMPORTADA EN EL SRC */}
         <img src={miFotoLocal} alt="Avatar" className="avatar-img" />
 
         {/* Los textos del screenshot */}
         <h1 className="title">Evaluación parcial 1</h1>
         
         {/* Nota: Puse "Feranda" tal cual la imagen, pero si es "Fernanda" solo corrígelo aquí jiji */}
-        <h2 className="subtitle">Alumno(a): Eduardo Chavez Guerrero</h2>
+        <h2 className="subtitle">Alumno(a): Eduardo Chavez Guerrerodua</h2>
 
         {/* Los links */}
         <div className="links-container">
@@ -28,16 +28,22 @@ function App() {
           >
             LINKED IN DE MI PROFILE
           </a>
-
           <a 
             href={process.env.PUBLIC_URL + '/documentacion.html'}
             className="link-text"
           >
             DOCUMENTACION PARCIAL 1
           </a>
+          <a 
+            href={process.env.PUBLIC_URL + '/documentacion2.html'}
+            className="link-text"
+          >
+            DOCUMENTACION PARCIAL 2
+          </a>
         </div>
-
+        {showMetodologias && <Metodologias />}
       </div>
     </div>
   );
-} export default App;
+}
+export default App;
